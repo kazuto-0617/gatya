8d6d1ea64badb7853f72ebcea3fde0ae0e453651
kazuto-0617 <kazto0617@gmail.com>
2022-02-10 13:47:02 +0900

    master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>ガチャ</h1>
+    <div v-if="resultShow">
+      <p>{{ item1 }}</p>
+      <p>{{ item2 }}</p>
+      <p>{{ item3 }}</p>
+    </div>
+    <button @click="turnTheGatya">回す</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      item1: "商品1",
+      item2: "商品2",
+      item3: "商品3",
+      resultShow: false,
+    };
+  },
+  methods: {
+    turnTheGatya() {
+      this.item1 = "商品5";
+      this.resultShow = true;
+    },
   },
 };
 </script>
